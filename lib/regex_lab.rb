66a42b6e78906b360dd62b2f4c84ae47f242f98e
Ben Spotswood <spotswoodb@gmail.com>
2020-11-25 13:07:27 -0600

    commit tree 0461e5028984a05e63bdb29208f059f9c1feb9e3

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+    # !!word.match(/\A[aeiouAEIOU]/)
+    if word.match(/\A[aeiouAEIOU]/)
+        return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/^[A-Z].*\W$/)
+        return true
+    else
+        return false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    !!phone.match(/\D?(\d{3}).?(\d{3}).?(\d{4})/)
 end
